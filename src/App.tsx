784a177e7c3186f4188465c7db84233edc4b2530
Mahmoud Fawzy Taha-Elaraby <1998mft1998@gmail.com>
2026-04-05 12:20:44 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,6 @@ import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import Layout from "@/components/layout/Layout";
 import Index from "./pages/Index";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Services from "./pages/Services";
-import Documents from "./pages/Documents";
-import Contact from "./pages/Contact";
-import CV from "./pages/CV";
 import XmlSchema from "./pages/XmlSchema";
 import XmlSchemaLanding from "./pages/XmlSchemaLanding";
 import NotFound from "./pages/NotFound";
@@ -24,54 +18,11 @@ const App = () => (
       {/* HashRouter works better with GitHub Pages for SPA routing */}
       <HashRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route
-            path="/about"
+          <Route 
+            path="/" 
             element={
-              <Layout>
-                <About />
-              </Layout>
-            }
-          />
-          <Route
-            path="/projects"
-            element={
-              <Layout>
-                <Projects />
-              </Layout>
-            }
-          />
-          <Route
-            path="/services"
-            element={
-              <Layout>
-                <Services />
-              </Layout>
-            }
-          />
-          <Route
-            path="/documents"
-            element={
-              <Layout>
-                <Documents />
-              </Layout>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <Layout>
-                <Contact />
-              </Layout>
-            }
-          />
-          <Route
-            path="/cv"
-            element={
-              <Layout>
-                <CV />
-              </Layout>
-            }
+              <Index />
+            } 
           />
           <Route
             path="/xml"
@@ -80,7 +31,7 @@ const App = () => (
                 <XmlSchemaLanding />
               </Layout>
             }
-          />
+          />          
           <Route
             path="/xml/:file"
             element={
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,12 @@
 
 import React from 'react';
 import Layout from '@/components/layout/Layout';
-import Home from './Home';
+import XmlSchemaLanding from './XmlSchemaLanding';
 
 const Index: React.FC = () => {
   return (
     <Layout>
-      <Home />
+      <XmlSchemaLanding />
     </Layout>
   );
 };
