9ba71e0fe5774bfc0bb8cc3b90d2d077f90d19d4
Mahmoud Fawzy Taha-Elaraby <1998mft1998@gmail.com>
2026-04-05 15:19:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/mft1998-portfolio/',
+  base: '/XML-Schema-viewer/',
   server: {
     host: "::",
     port: 8080,
